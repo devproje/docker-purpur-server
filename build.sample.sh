e66c91a728cbbade0e6139147c9f4387460851cb
devproje <me@projecttl.net>
2022-12-28 21:46:18 +0900

    feat: update minecraft version [ 1.19.2 => 1.19.3 ]

--- build.sample.sh
+++ build.sample.sh
@@ -1,1 +1,1 @@
-docker build --no-cache --tag docker-purpur-server:latest --build-arg VELOCITY_SECRET=SAMPLE_VELOCITY_SECRET .
+docker-compose build --no-cache --build-arg VELOCITY_SECRET=SAMPLE_VELOCITY_SECRET
